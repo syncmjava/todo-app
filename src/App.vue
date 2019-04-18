f7cba875be6fa7f5692b217e7209eecb4a73a61f
Sun <changming.sun@accenture.com>
2019-04-18 12:59:43 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
